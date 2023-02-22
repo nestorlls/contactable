13f2dbda63116c11867c00f2f781f21a7ae053dc
Paul Llanque <llanquepol@gmail.com>
2023-02-21 21:35:12 -0500

    add login function to API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Happy coding')
+// base uri API
+const BASE_URI = 'https://contactable-js-api.herokuapp.com/';
+
+// login
+async function login(credentials = { email, password }) {
+  const response = await fetch(`${BASE_URI}login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  const user = await response.json();
+  console.log(user);
+}
+
+const credentials = {
+  email: 'llanque@mail.com',
+  password: '123456',
+};
+
+login(credentials);
