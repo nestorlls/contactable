7b644c018b8e8c840c7b7358a1df47ca4a378f9c
Paul Llanque <llanquepol@gmail.com>
2023-02-21 21:40:50 -0500

    ADD: user services and config url API

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// base uri API
-const BASE_URI = 'https://contactable-js-api.herokuapp.com/';
+import { login } from "./src/services/user-services.js";
 
-// login
-async function login(credentials = { email, password }) {
-  const response = await fetch(`${BASE_URI}login`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(credentials),
-  });
-
-  const user = await response.json();
-  console.log(user);
-}
 
 const credentials = {
   email: 'llanque@mail.com',
---
+++ src/config.js
@@ -0,0 +1,2 @@
+// base uri API
+export const BASE_URI = 'https://contactable-js-api.herokuapp.com/';
